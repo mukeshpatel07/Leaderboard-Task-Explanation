17d6a36deb5524aebdffc859dcb37372a9730021
mukeshpatel07 <157115384+mukeshpatel07@users.noreply.github.com>
2025-07-15 16:12:38 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { getUsers, claimPoints, getLeaderboard } from './api';
+import AddUserForm from './components/AddUserForm';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [selectedUserId, setSelectedUserId] = useState('');
+  const [leaderboard, setLeaderboard] = useState([]);
+  const [claimedPoints, setClaimedPoints] = useState(null);
+
+  const refreshUsers = async () => {
+    const res = await getUsers();
+    setUsers(res.data);
+  };
+
+  const refreshLeaderboard = async () => {
+    const res = await getLeaderboard();
+    setLeaderboard(res.data);
+  };
+
+  useEffect(() => {
+    refreshUsers();
+    refreshLeaderboard();
+  }, []);
+
+  const handleClaim = async () => {
+    if (!selectedUserId) return alert('Select a user first');
+    const res = await claimPoints(selectedUserId);
+    setClaimedPoints(res.data.randomPoints);
+    refreshUsers();
+    refreshLeaderboard();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Home
+              users={users}
+              selectedUserId={selectedUserId}
+              setSelectedUserId={setSelectedUserId}
+              handleClaim={handleClaim}
+              claimedPoints={claimedPoints}
+              leaderboard={leaderboard}
+              refreshUsers={refreshUsers} // pass refreshUsers to Home
+            />
+          }
+        />
+        <Route
+          path="/add-user"
+          element={<AddUserForm refreshUsers={refreshUsers} />}
+        />
+      </Routes>
+    </Router>
   );
 }
 
